6d494f08114dfa800e09cfd8706fb63b1ef3106d
GioeleBarbieri <gioelebarbieri48@gmail.com>
2024-09-27 21:17:32 +0200

    undicesimo esercizio

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    float prezzoUni, importoNoSconto, importo, sconto;
+    int numeroProdotti;
+    printf("Inserisci il numero di prodotti: ");
+    scanf("%d", &numeroProdotti);
+    printf("inserisci il prezzo unitario: ");
+    scanf("%f", &prezzoUni);
+    importoNoSconto = numeroProdotti * prezzoUni;
+    if (importoNoSconto > 10)
+    {
+        sconto=importoNoSconto * 0.10;
+        importo = importoNoSconto - sconto;
+    }
+    if (importoNoSconto > 50)
+    {
+        sconto=importoNoSconto * 0.20;
+        importo=importoNoSconto - sconto;
+    }
+    else (importo == importoNoSconto);
+    sconto = importoNoSconto - importo;
+    printf ("l'importo non scontato e' uguale a: %f", importoNoSconto);
+    printf ("l'importo scontato e' uguale a: %f", importo);
+    printf ("lo sconto e' di %f euro", sconto);
     return 0;
+
 }
